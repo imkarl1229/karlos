cf57ce785b362d7045530ec2324698ef66ee2cf9
imkarl1229 <in156family19@gmail.com>
2025-10-08 19:45:26 -0400

    Disable interrupt at the beginning before IDT setup

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,16 +1,16 @@
 #![no_std]
 #![no_main]
 
-mod asm_utils;
+mod arch;
 mod traits;
 
-use crate::asm_utils::disable_interrupt;
 use core::panic::PanicInfo;
 use shared::boot_info::BootInfo;
+use crate::arch::current::*;
 
 #[unsafe(no_mangle)]
 extern "sysv64" fn _start(boot_info_ptr: *const BootInfo) -> ! {
-    unsafe { disable_interrupt(); }
+    unsafe { disable_interrupt() }
     assert_ne!(core::ptr::null(), boot_info_ptr);
 
     let boot_info = unsafe { &mut *(boot_info_ptr as *mut BootInfo) };
